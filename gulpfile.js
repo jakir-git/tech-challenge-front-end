f413fcd36a9676cfe3604815336cbf0ee69e81b4
jakir-git <carrier.jakir@gmail.comm>
2024-07-14 02:08:33 +0600

    Modify gulpfile, upload build folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,53 @@
-const gulp         = require('gulp');
-const sass         = require('gulp-sass')(require('sass'));
-const browserSync  = require('browser-sync').create();
-const sourcemaps   = require('gulp-sourcemaps');
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const browserSync = require("browser-sync").create();
+const sourcemaps = require("gulp-sourcemaps");
 
 const includePaths = [
-    'node_modules/foundation-sites/scss',
-    'node_modules/motion-ui/src'
+  "node_modules/foundation-sites/scss",
+  "node_modules/motion-ui/src",
 ];
 
 function sassBuild() {
-    return gulp.src(['scss/app.scss'])
-        .pipe(sourcemaps.init())
-        .pipe(sass({
-            includePaths : includePaths,
-            outputStyle  : 'compressed'
-        }).on('error', sass.logError))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('css'));
-};
-
-function serve() {
-    browserSync.init({server : "./"});
-    gulp.watch("scss/*.scss", sassBuild);
-    gulp.watch("*.html").on('change', browserSync.reload);
+  return gulp
+    .src(["scss/app.scss"])
+    .pipe(sourcemaps.init())
+    .pipe(
+      sass({
+        includePaths: includePaths,
+        outputStyle: "compressed",
+      }).on("error", sass.logError)
+    )
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("public/css")); // Output to public directory
 }
 
-gulp.task('sass', sassBuild);
-gulp.task('serve', gulp.series('sass', serve));
-gulp.task('default', gulp.series('sass', serve));
+function copyHtml() {
+  return gulp.src("index.html").pipe(gulp.dest("public"));
+}
+
+function copyAssets() {
+  return gulp
+    .src(["images/**/*", "js/**/*", "webfonts/**/*"], { base: "./" })
+    .pipe(gulp.dest("public"));
+}
+
+function serve() {
+  browserSync.init({ server: "./public" });
+  gulp.watch("scss/*.scss", gulp.series(sassBuild, reload));
+  gulp.watch("index.html", gulp.series(copyHtml, reload));
+  gulp.watch("js/*.js", gulp.series(copyAssets, reload));
+}
+
+function reload(done) {
+  browserSync.reload();
+  done();
+}
+
+gulp.task("sass", sassBuild);
+gulp.task("html", copyHtml);
+gulp.task("assets", copyAssets);
+gulp.task("serve", gulp.series("sass", "html", "assets", serve));
+gulp.task("default", gulp.series("sass", "html", "assets", serve));
+
+gulp.task("build", gulp.series("sass", "html", "assets"));
